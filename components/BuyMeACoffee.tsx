dc17a6bb582907ae1678029c2c4d59a2b12ad2be
seedpower <11022830+seedpower@users.noreply.github.com>
2026-07-06 11:05:11 +0800

    add a donation tip title

--- components/BuyMeACoffee.tsx
+++ components/BuyMeACoffee.tsx
@@ -12,7 +12,9 @@ export default function BuyMeACoffee() {
   const buttonImageUrl = `https://img.buymeacoffee.com/button-api/?text=Buy%20me%20a%20coffee&emoji=%E2%98%95&slug=${slug}&button_colour=FFDD00&font_colour=000000&font_family=Lato&outline_colour=000000&coffee_colour=ffffff`
 
   return (
-    <div className="my-8 flex justify-center">
+    <div className="flex flex-col items-center justify-center">
+      <div className="text-center text-gray-700 dark:text-gray-300 my-2">如果文章内容对你有帮助，欢迎打赏支持作者继续创作</div>
+      <div className="">
       <a
         href={buymeacoffee}
         target="_blank"
@@ -22,6 +24,7 @@ export default function BuyMeACoffee() {
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src={buttonImageUrl} alt="Buy me a coffee" width={217} height={60} />
       </a>
+      </div>
     </div>
   )
 }
